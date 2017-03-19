ce9ebf978c6d73bcdd7940d325f784df4bd7293c
Akhilesh Tharakan <akhilesh.tharakan@gmail.com>
2017-03-19 23:04:38 +0530

    Update

---
+++ app_s6_a1.js
@@ -0,0 +1,11 @@
+/* This is the script file from where we are passing the value */	
+/* Session 6 Assignment 1 */
+	
+var radius = parseInt(prompt("Enter value for radius: "));
+console.log("Radius is: " + radius);
+radius **=2;
+var areaofcircle = 3.14159* radius;
+
+console.log("Area of the circle is: " + areaofcircle);
+alert("Area of the circle is: " + areaofcircle);
+	
